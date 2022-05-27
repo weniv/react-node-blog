1edb467299af9b9f0c2c06f1cf9860145a7a33c5
강혜진 <dreamfulbud@naver.com>
2022-05-27 16:19:06 +0900

    ✨ NotFound 페이지 추가

---
+++ src/templates/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Button from "../components/button/Button";
+
+export default function NotFound() {
+	return (
+		<>
+			<section className="max-width not-found">
+				<h2>OOPS! : (</h2>
+				<p> We can't find the page</p>
+				<Button href="/">HOME</Button>
+			</section>
+		</>
+	);
+}
