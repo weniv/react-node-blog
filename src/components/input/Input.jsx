00cbb6f9aa9072ba804afe01af5f19c6ef23abbb
강혜진 <dreamfulbud@naver.com>
2022-05-27 16:16:08 +0900

    ✅ required, minLength props 추가

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 
 import "./input.css";
 
-export default function Input({ label, type, placeholder, id, value }) {
+export default function Input({ label, type, placeholder, id, value, required, minLength }) {
 	const [newValue, setNewValue] = useState(value);
 	return (
 		<>
 			<label htmlFor={id}>{label}</label>
-			<input type={type} id={id} placeholder={placeholder} value={newValue} onChange={(e) => setNewValue(e.target.value)} />
+			<input type={type} id={id} placeholder={placeholder} value={newValue} onChange={(e) => setNewValue(e.target.value)} required={required} minLength={minLength} />
 		</>
 	);
 }
