950f2de4526e7de571c3c6756ea99428484430cb
강혜진 <dreamfulbud@naver.com>
2022-06-09 16:59:56 +0900

    📝 게시글 페이지 배경 이미지 처리

--- src/components/banner/BannerWrap.jsx
+++ src/components/banner/BannerWrap.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./banner.css";
 
-export default function BannerWrap({ children }) {
+export default function BannerWrap({ children, mainBg }) {
 	return (
-		<div className="banner">
+		<div className="banner" style={mainBg && { backgroundImage: `url(${process.env.PUBLIC_URL + mainBg})` }}>
 			<div className="max-width">{children}</div>
 		</div>
 	);
--- src/components/banner/PostBanner.jsx
+++ src/components/banner/PostBanner.jsx
@@ -6,9 +6,10 @@ export default function PostBanner({ post }) {
 	const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 	const postDate = new Date(post.created);
+	const mainBg = post.mainBg;
 
 	return (
-		<BannerWrap>
+		<BannerWrap mainBg={mainBg}>
 			<div className="banner-contents">
 				<p className="today">
 					{month[postDate.getMonth()]} <em>{String(postDate.getDate()).padStart(2, "0")}</em> {week[postDate.getDay()]}
